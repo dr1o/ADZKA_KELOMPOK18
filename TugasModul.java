b3f80ef95d0d2de1ed1d4d8eec6ce92b8f0e2c7f
dr1o <drio6077@gmail.com>
2024-10-02 10:56:27 +0700

    Update TugasModul.java

--- TugasModul.java
+++ TugasModul.java
@@ -20,8 +20,31 @@ public class TugasModul4 {
     static void pilihMenu() {
         String[] pilihan = {"paket 1: ayam bakar","paket 2: ayam goreng", "paket 3: ayam bali",
                 "paket 4: ayam bakar + es teh manis","paket 5: ayam goreng + teh hangat"};
+    public void bayar() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("masukan jumlah pesanan (masukan angka): ");
+        int angka = input.nextInt();
+        input.nextLine();
+        
+        int[] harga = {13000,12000,13000,15000,15000};
+        int[] menu = new int[angka];
+        int totalbayar = 0;
+        for (int i = 0;i<angka;i++){
+            System.out.print("pilih menu ke-" + (i+1)+ "(masukan nomor paket):");
+            menu[i] = input.nextInt();
+            input.nextLine();
+            if(menu[i]>5){
+                System.out.print("menu paket tidak tersedia, input lagi menu ke-" + (i+1)+ "(masukan nomor paket):");
+                menu[i] = input.nextInt();
+            }
+            totalbayar += harga[menu[i]-1];
+        }
 
-        System.out.println(Arrays.toString(pilihan));
+        System.out.println("pilihan paket makanan anda: " + Arrays.toString(menu));
+        System.out.println("total bayar: " + totalbayar);
     }
 
-}
+    public String intro(String nama, int waktu) {
+        return "terima kasih " + nama + " karena sudah memesan, pesanan anda akan segera diproses dalam "
+                + waktu + " menit";
+    }
