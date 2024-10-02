bacfa0df8a05dc4c08def6b29e4bd1ecc9ede443
adzkaas <abdurrahmanadzka@gmail.com>
2024-10-02 10:29:27 +0700

    TugasLaprak4

---
+++ TugasModul.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class TugasModul4 {
+    //adzka
+    static void salam(String Halo) {
+        System.out.println("Salam :" + Halo);
+    }
+
+    static String returnText() {
+        return "Selamat datang di burjo 18";
+    }
+
+    static String returnParam(String text) {
+        return text;
+    }
+
+    static void pilihMenu() {
+        String[] pilihan = {"paket 1: ayam bakar","paket 2: ayam goreng", "paket 3: ayam bali",
+                "paket 4: ayam bakar + es teh manis","paket 5: ayam goreng + teh hangat"};
+
+        System.out.println(Arrays.toString(pilihan));
+    }
+
+}
